f176d8e560b0ababf5ead4d6a0a603ca611dd0cb
Marco Massarelli <60667061+ceoloide@users.noreply.github.com>
2024-04-02 17:50:30 +0000

    Fix module name

--- battery_connector_jst_ph_2.js
+++ battery_connector_jst_ph_2.js
@@ -94,7 +94,7 @@ module.exports = {
         ];
 
         const standard_opening = `
-            (module "ceoloide/battery_connector_jst_ph_2" (layer ${p.side}.Cu) (tedit 6135B927)
+            (module "ceoloide:battery_connector_jst_ph_2" (layer ${p.side}.Cu) (tedit 6135B927)
                 ${p.at /* parametric position */}
 
                 (descr "JST PH series connector, S2B-PH-K (http://www.jst-mfg.com/product/pdf/eng/ePH.pdf)")
