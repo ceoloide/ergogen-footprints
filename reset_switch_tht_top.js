ac381a03217f83de250b499728fb5a620c5fde27
Marco Massarelli <marco.massarelli@gmail.com>
2024-04-12 16:28:21 -0400

    Upgrade `reset_switch_tht_top.js` to KiCad 8 (#18)

--- reset_switch_tht_top.js
+++ reset_switch_tht_top.js
@@ -24,57 +24,61 @@
 //      if true it will include silkscreen markings
 
 module.exports = {
-    params: {
-        designator: 'RST', // for Button
-        side: 'F',
-        reversible: false,
-        include_silkscreen: true,
-        from: { type: 'net', value: 'GND' },
-        to: { type: 'net', value: 'RST' },
-    },
-    body: p => {
-        const common_start = `
-            (module "ceoloide:reset_switch_tht_top" (layer ${p.side}.Cu) (tedit 5B9559E6) (tstamp 61905781)
-                ${p.at /* parametric position */}
-                (fp_text value "reset_switch_tht_top" (at 0 -2.55 ${90 + p.r}) (layer ${p.side}.Fab) (effects (font (size 1 1) (thickness 0.15))))
-                (fp_text reference "${p.ref}" (at 0 2.55 ${90 + p.r}) (layer ${p.side}.SilkS) ${p.ref_hide} (effects (font (size 1 1) (thickness 0.15))))
-            `
-        const silkscreen_front = `
-                (fp_text user "RST" (at 0 0 ${p.r}) (layer F.SilkS) (effects (font (size 1 1) (thickness 0.15))))
-                (fp_line (start -3 1.75) (end 3 1.75) (layer F.SilkS) (width 0.15))
-                (fp_line (start 3 1.75) (end 3 1.5) (layer F.SilkS) (width 0.15))
-                (fp_line (start -3 1.75) (end -3 1.5) (layer F.SilkS) (width 0.15))
-                (fp_line (start -3 -1.75) (end -3 -1.5) (layer F.SilkS) (width 0.15))
-                (fp_line (start -3 -1.75) (end 3 -1.75) (layer F.SilkS) (width 0.15))
-                (fp_line (start 3 -1.75) (end 3 -1.5) (layer F.SilkS) (width 0.15))
-            `
-        const silkscreen_back = `
-                (fp_text user "RST" (at 0 0 ${p.r}) (layer B.SilkS) (effects (font (size 1 1) (thickness 0.15)) (justify mirror)))
-                (fp_line (start 3 1.5) (end 3 1.75) (layer B.SilkS) (width 0.15))
-                (fp_line (start 3 1.75) (end -3 1.75) (layer B.SilkS) (width 0.15))
-                (fp_line (start -3 1.75) (end -3 1.5) (layer B.SilkS) (width 0.15))
-                (fp_line (start -3 -1.5) (end -3 -1.75) (layer B.SilkS) (width 0.15))
-                (fp_line (start -3 -1.75) (end 3 -1.75) (layer B.SilkS) (width 0.15))
-                (fp_line (start 3 -1.75) (end 3 -1.5) (layer B.SilkS) (width 0.15))
-            `
-        const common_end = `
-                (pad 2 thru_hole circle (at -3.25 0 ${p.r}) (size 2 2) (drill 1.3) (layers *.Cu *.Mask) ${p.from.str})
-                (pad 1 thru_hole circle (at 3.25 0 ${p.r}) (size 2 2) (drill 1.3) (layers *.Cu *.Mask) ${p.to.str})
-            )
+  params: {
+    designator: 'RST', // for Button
+    side: 'F',
+    reversible: false,
+    include_silkscreen: true,
+    from: { type: 'net', value: 'GND' },
+    to: { type: 'net', value: 'RST' },
+  },
+  body: p => {
+    const common_start = `
+  (module "ceoloide:reset_switch_tht_top"
+    (layer ${p.side}.Cu)
+    ${p.at}
+    (property "Reference" "${p.ref}"
+      (at 0 2.55 ${90 + p.r})
+      (layer "${p.side}.SilkS")
+      ${p.ref_hide}
+      (effects (font (size 1 1) (thickness 0.15)))
+    )
         `
-
-        let final = common_start;
-        if (p.side == "F" || p.reversible) {
-            if (p.include_silkscreen) {
-                final += silkscreen_front
-            }
-        }
-        if (p.side == "B" || p.reversible) {
-            if (p.include_silkscreen) {
-                final += silkscreen_back
-            }
-        }
-        final += common_end;
-        return final;
+    const silkscreen_front = `
+    (fp_text user "RST" (at 0 0 ${p.r}) (layer "F.SilkS") (effects (font (size 1 1) (thickness 0.15))))
+    (fp_line (start -3 1.75) (end 3 1.75) (layer "F.SilkS") (stroke (width 0.15) (type solid)))
+    (fp_line (start 3 1.75) (end 3 1.5) (layer "F.SilkS") (stroke (width 0.15) (type solid)))
+    (fp_line (start -3 1.75) (end -3 1.5) (layer "F.SilkS") (stroke (width 0.15) (type solid)))
+    (fp_line (start -3 -1.75) (end -3 -1.5) (layer "F.SilkS") (stroke (width 0.15) (type solid)))
+    (fp_line (start -3 -1.75) (end 3 -1.75) (layer "F.SilkS") (stroke (width 0.15) (type solid)))
+    (fp_line (start 3 -1.75) (end 3 -1.5) (layer "F.SilkS") (stroke (width 0.15) (type solid)))
+        `
+    const silkscreen_back = `
+    (fp_text user "RST" (at 0 0 ${p.r}) (layer "B.SilkS") (effects (font (size 1 1) (thickness 0.15)) (justify mirror)))
+    (fp_line (start 3 1.5) (end 3 1.75) (layer "B.SilkS") (stroke (width 0.15) (type solid)))
+    (fp_line (start 3 1.75) (end -3 1.75) (layer "B.SilkS") (stroke (width 0.15) (type solid)))
+    (fp_line (start -3 1.75) (end -3 1.5) (layer "B.SilkS") (stroke (width 0.15) (type solid)))
+    (fp_line (start -3 -1.5) (end -3 -1.75) (layer "B.SilkS") (stroke (width 0.15) (type solid)))
+    (fp_line (start -3 -1.75) (end 3 -1.75) (layer "B.SilkS") (stroke (width 0.15) (type solid)))
+    (fp_line (start 3 -1.75) (end 3 -1.5) (layer "B.SilkS") (stroke (width 0.15) (type solid)))
+        `
+    const common_end = `
+    (pad "2" thru_hole circle (at -3.25 0 ${p.r}) (size 2 2) (drill 1.3) (layers "*.Cu" "*.Mask") ${p.from.str})
+    (pad "1" thru_hole circle (at 3.25 0 ${p.r}) (size 2 2) (drill 1.3) (layers "*.Cu" "*.Mask") ${p.to.str})
+  )
+        `
+    let final = common_start;
+    if (p.side == "F" || p.reversible) {
+      if (p.include_silkscreen) {
+        final += silkscreen_front
+      }
     }
+    if (p.side == "B" || p.reversible) {
+      if (p.include_silkscreen) {
+        final += silkscreen_back
+      }
+    }
+    final += common_end;
+    return final;
+  }
 }
