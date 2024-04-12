91cf640cb99ecf26842223a27d8dfa9af6f20320
Marco Massarelli <marco.massarelli@gmail.com>
2024-04-12 14:29:28 -0400

    Upgrade diode to KiCad 8 (#18)

--- diode_tht_sod123.js
+++ diode_tht_sod123.js
@@ -41,6 +41,7 @@
 //
 // @ceoloide's improvements:
 //  - Add single side support
+//  - Upgrade to KiCad 8
 //
 // @grazfather's improvements:
 //  - Add support for switch 3D model
@@ -55,48 +56,54 @@ module.exports = {
         diode_3dmodel_xyz_offset: [0, 0, 0],
         diode_3dmodel_xyz_rotation: [0, 0, 0],
         diode_3dmodel_xyz_scale: [1, 1, 1],
-        from: undefined,
-        to: undefined
+        from: { type: 'net', value: undefined },
+        to: { type: 'net', value: undefined }
     },
     body: p => {
         const standard_opening = `
-        (module "ceoloide:diode_tht_sod123" (layer ${p.side}.Cu) (tedit 5B24D78E)
-            ${p.at /* parametric position */}
-            (fp_text reference "${p.ref}" (at 0 0 ${p.r}) (layer ${p.side}.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
+    (footprint "ceoloide:diode_tht_sod123"
+        (layer "${p.side}.Cu")
+        ${p.at}
+        (property "Reference" "${p.ref}"
+            (at 0 0 ${p.r})
+            (layer "${p.side}.SilkS")
+            ${p.ref_hide}
+            (effects (font (size 1 1) (thickness 0.15)))
+        )
         `
         const front = `
-            (fp_line (start 0.25 0) (end 0.75 0) (layer F.SilkS) (width 0.1))
-            (fp_line (start 0.25 0.4) (end -0.35 0) (layer F.SilkS) (width 0.1))
-            (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer F.SilkS) (width 0.1))
-            (fp_line (start -0.35 0) (end 0.25 -0.4) (layer F.SilkS) (width 0.1))
-            (fp_line (start -0.35 0) (end -0.35 0.55) (layer F.SilkS) (width 0.1))
-            (fp_line (start -0.35 0) (end -0.35 -0.55) (layer F.SilkS) (width 0.1))
-            (fp_line (start -0.75 0) (end -0.35 0) (layer F.SilkS) (width 0.1))
-            (pad 1 smd rect (at -1.65 0 ${p.r}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.to.str})
-            (pad 2 smd rect (at 1.65 0 ${p.r}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.from.str})
+        (fp_line (start 0.25 0) (end 0.75 0) (layer "F.SilkS") (width 0.1))
+        (fp_line (start 0.25 0.4) (end -0.35 0) (layer "F.SilkS") (width 0.1))
+        (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer "F.SilkS") (width 0.1))
+        (fp_line (start -0.35 0) (end 0.25 -0.4) (layer "F.SilkS") (width 0.1))
+        (fp_line (start -0.35 0) (end -0.35 0.55) (layer "F.SilkS") (width 0.1))
+        (fp_line (start -0.35 0) (end -0.35 -0.55) (layer "F.SilkS") (width 0.1))
+        (fp_line (start -0.75 0) (end -0.35 0) (layer "F.SilkS") (width 0.1))
+        (pad 1 smd rect (at -1.65 0 ${p.r}) (size 0.9 1.2) (layers "F.Cu" "F.Paste" "F.Mask") ${p.to.str})
+        (pad 2 smd rect (at 1.65 0 ${p.r}) (size 0.9 1.2) (layers "F.Cu" "F.Paste" "F.Mask") ${p.from.str})
         `
         const back = `
-            (fp_line (start 0.25 0) (end 0.75 0) (layer B.SilkS) (width 0.1))
-            (fp_line (start 0.25 0.4) (end -0.35 0) (layer B.SilkS) (width 0.1))
-            (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer B.SilkS) (width 0.1))
-            (fp_line (start -0.35 0) (end 0.25 -0.4) (layer B.SilkS) (width 0.1))
-            (fp_line (start -0.35 0) (end -0.35 0.55) (layer B.SilkS) (width 0.1))
-            (fp_line (start -0.35 0) (end -0.35 -0.55) (layer B.SilkS) (width 0.1))
-            (fp_line (start -0.75 0) (end -0.35 0) (layer B.SilkS) (width 0.1))
-            (pad 2 smd rect (at 1.65 0 ${p.r}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.from.str})
-            (pad 1 smd rect (at -1.65 0 ${p.r}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.to.str})
+        (fp_line (start 0.25 0) (end 0.75 0) (layer "B.SilkS") (width 0.1))
+        (fp_line (start 0.25 0.4) (end -0.35 0) (layer "B.SilkS") (width 0.1))
+        (fp_line (start 0.25 -0.4) (end 0.25 0.4) (layer "B.SilkS") (width 0.1))
+        (fp_line (start -0.35 0) (end 0.25 -0.4) (layer "B.SilkS") (width 0.1))
+        (fp_line (start -0.35 0) (end -0.35 0.55) (layer "B.SilkS") (width 0.1))
+        (fp_line (start -0.35 0) (end -0.35 -0.55) (layer "B.SilkS") (width 0.1))
+        (fp_line (start -0.75 0) (end -0.35 0) (layer "B.SilkS") (width 0.1))
+        (pad 2 smd rect (at 1.65 0 ${p.r}) (size 0.9 1.2) (layers "B.Cu" "B.Paste" "B.Mask") ${p.from.str})
+        (pad 1 smd rect (at -1.65 0 ${p.r}) (size 0.9 1.2) (layers "B.Cu" "B.Paste" "B.Mask") ${p.to.str})
         `
 
         const tht = `
-            (pad 1 thru_hole rect (at -3.81 0 ${p.r}) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${p.to.str})
-            (pad 2 thru_hole circle (at 3.81 0 ${p.r}) (size 1.905 1.905) (drill 0.9906) (layers *.Cu *.Mask) ${p.from.str})
+        (pad 1 thru_hole rect (at -3.81 0 ${p.r}) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${p.to.str})
+        (pad 2 thru_hole circle (at 3.81 0 ${p.r}) (size 1.905 1.905) (drill 0.9906) (layers *.Cu *.Mask) ${p.from.str})
         `
 
         const diode_3dmodel = `
-            (model ${p.diode_3dmodel_filename}
-                (offset (xyz ${p.diode_3dmodel_xyz_offset[0]} ${p.diode_3dmodel_xyz_offset[1]} ${p.diode_3dmodel_xyz_offset[2]}))
-                (scale (xyz ${p.diode_3dmodel_xyz_scale[0]} ${p.diode_3dmodel_xyz_scale[1]} ${p.diode_3dmodel_xyz_scale[2]}))
-                (rotate (xyz ${p.diode_3dmodel_xyz_rotation[0]} ${p.diode_3dmodel_xyz_rotation[1]} ${p.diode_3dmodel_xyz_rotation[2]})))
+        (model ${p.diode_3dmodel_filename}
+            (offset (xyz ${p.diode_3dmodel_xyz_offset[0]} ${p.diode_3dmodel_xyz_offset[1]} ${p.diode_3dmodel_xyz_offset[2]}))
+            (scale (xyz ${p.diode_3dmodel_xyz_scale[0]} ${p.diode_3dmodel_xyz_scale[1]} ${p.diode_3dmodel_xyz_scale[2]}))
+            (rotate (xyz ${p.diode_3dmodel_xyz_rotation[0]} ${p.diode_3dmodel_xyz_rotation[1]} ${p.diode_3dmodel_xyz_rotation[2]})))
         `
         const standard_closing = `
         )
