8b0c3f9e7b387d3d3abbc7b182ff82ecd19f7832
Ivan Koz <dev.koz@yandex.ru>
2024-04-23 15:38:48 +0400

    Add `allow_soldermask_bridges` option, which disables `solder mask aperture bridges items with different nets` DRC check

--- switch_choc_v1_v2.js
+++ switch_choc_v1_v2.js
@@ -68,6 +68,9 @@
 //      to show a 1.5u outline for easier aligning.
 //    keycap_width: default is 18
 //      Allows you to adjust the height of the keycap outline.
+//    allow_soldermask_bridges: default is true
+//      Disables 'solder mask aperture bridges items with different nets' DRC check when set to true
+//      setting this option to false may be useful for debugging purposes
 //    switch_3dmodel_filename: default is ''
 //      Allows you to specify the path to a 3D model STEP or WRL file to be
 //      used when rendering the PCB. Use the ${VAR_NAME} syntax to point to
@@ -126,6 +129,7 @@
 //  - Add ability to set a net to central hole (useful for connecting ground fill zones)
 //  - Add opposite stabilizer / boss holes when (choc_v2_support & solder & hotswap) options enabled together
 //  - Change v2 stabilizer / boss holes to plated
+//  - Add allow_soldermask_bridges option, which disables 'solder mask aperture bridges items with different nets' DRC check
 
 module.exports = {
   params: {
@@ -148,6 +152,7 @@ module.exports = {
     choc_v1_support: true,
     choc_v2_support: true,
     choc_v1_stabilizers_diameter: 1.9,
+    allow_soldermask_bridges: true,
     switch_3dmodel_filename: '',
     switch_3dmodel_xyz_offset: [0, 0, 0],
     switch_3dmodel_xyz_rotation: [0, 0, 0],
@@ -173,7 +178,7 @@ module.exports = {
       ${p.ref_hide}
       (effects (font (size 1 1) (thickness 0.15)))
     )
-    (attr exclude_from_pos_files exclude_from_bom allow_soldermask_bridges)
+    (attr exclude_from_pos_files exclude_from_bom${allow_soldermask_bridges ? ' allow_soldermask_bridges' : ''})
 
     ${''/* middle shaft hole */}
     ${p.include_plated_holes ? `
