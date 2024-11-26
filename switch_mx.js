1ab68faeebc4989e07040f75c09f7ec9ae6fdd35
Marco Massarelli <marco.massarelli@gmail.com>
2024-11-26 09:53:02 -0500

    Add plated mounting holes and related nets

--- switch_mx.js
+++ switch_mx.js
@@ -28,6 +28,17 @@ Params:
     if true, will include holes and pads for Kailh choc hotswap sockets
   solder: default is false
     if true, will include holes to solder switches (works with hotswap too)
+  include_plated_holes: default is false
+    Alternate version of the footprint compatible with side, reversible, hotswap, solder
+    options in any combination. Pretty, allows for connecting ground fill zones via
+    center hole, and allows for setting nets on stabilizers for your routing needs.
+  include_stabilizer_nets: default is false
+    if true, will add adjustable nets to plated stabilizer holes, 
+    LEFTSTAB: default is "D1"
+    RIGHTSTAB: default is "D2"
+  include_centerhole_net: default is false
+    if true, will add adjustable net to the center hole
+    CENTERHOLE: default is "GND"
   outer_pad_width_front: default 2.6
   outer_pad_width_back: default 2.6
     Allows you to make the outer hotswap pads smaller to silence DRC
@@ -99,6 +110,9 @@ module.exports = {
     side: 'B',
     reversible: false,
     hotswap: true,
+    include_plated_holes: false,
+    include_stabilizer_nets: false,
+    include_centerhole_net: false,
     solder: false,
     outer_pad_width_front: 2.6,
     outer_pad_width_back: 2.6,
@@ -122,7 +136,10 @@ module.exports = {
     keycap_3dmodel_xyz_rotation: [0, 0, 0],
     keycap_3dmodel_xyz_scale: [1, 1, 1],
     from: undefined,
-    to: undefined
+    to: undefined,
+    CENTERHOLE: { type: 'net', value: 'GND'},
+    LEFTSTAB: { type: 'net', value: 'D1' },
+    RIGHTSTAB: { type: 'net', value: 'D2' }
   },
   body: p => {
     const common_top = `
@@ -136,9 +153,27 @@ module.exports = {
       (effects (font (size 1 1) (thickness 0.15)))
     )
     
-    (pad "" np_thru_hole circle (at 0 0 90) (size 4.1 4.1) (drill 4.1) (layers "*.Cu" "*.Mask"))
-    (pad "" np_thru_hole circle (at 5.08 0 180) (size ${p.stabilizers_diameter} ${p.stabilizers_diameter}) (drill ${p.stabilizers_diameter}) (layers "*.Cu" "*.Mask"))
-    (pad "" np_thru_hole circle (at -5.08 0 180) (size ${p.stabilizers_diameter} ${p.stabilizers_diameter}) (drill ${p.stabilizers_diameter}) (layers "*.Cu" "*.Mask"))
+    (pad "" ${!p.include_plated_holes ? `np_thru_hole` : `thru_hole`} circle 
+      (at 0 0 ${p.r})
+      (size ${p.include_plated_holes ? `4.4 4.4` : `4.1 4.1`})
+      (drill 4.1)
+      (layers "*.Cu" "*.Mask")
+      ${p.include_plated_holes && p.include_centerhole_net ? p.CENTERHOLE : ''}
+    )
+    (pad "" ${!p.include_plated_holes ? `np_thru_hole` : `thru_hole`} circle 
+      (at 5.08 0 ${p.r})
+      (size ${p.stabilizers_diameter + (p.include_plated_holes ? 0.3 : 0)} ${p.stabilizers_diameter + (p.include_plated_holes ? 0.3 : 0)})
+      (drill ${p.stabilizers_diameter})
+      (layers "*.Cu" "*.Mask")
+      ${p.include_plated_holes && p.include_centerhole_net ? p.RIGHTSTAB : ''}
+    )
+    (pad "" ${!p.include_plated_holes ? `np_thru_hole` : `thru_hole`} circle 
+      (at -5.08 0 ${p.r})
+      (size ${p.stabilizers_diameter + (p.include_plated_holes ? 0.3 : 0)} ${p.stabilizers_diameter + (p.include_plated_holes ? 0.3 : 0)})
+      (drill ${p.stabilizers_diameter})
+      (layers "*.Cu" "*.Mask")
+      ${p.include_plated_holes && p.include_centerhole_net ? p.LEFTSTAB : ''}
+    )
     `
     const corner_marks = `
     (fp_line (start -7 -6) (end -7 -7) (layer "Dwgs.User") (stroke (width 0.15) (type solid)))
@@ -182,7 +217,8 @@ module.exports = {
 			(roundrect_rratio 0)
 			(chamfer_ratio 0.2)
 			(chamfer bottom_left)` : ''}
-      ${p.to})
+      ${p.to}
+    )
     `
 
     const hotswap_silkscreen_back = `
